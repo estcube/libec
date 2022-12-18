8318dfc480b9e365000c9fa69fd3c26c77c496e7
nowittyusername <nowittyusername@users.noreply.github.com>
2022-12-18 21:19:20 +0200

    Fix endianess
    The ec::error tyoe is now properly little endian.

--- inc/error.hpp
+++ inc/error.hpp
@@ -7,39 +7,39 @@ namespace ec {
 
 class error {
 public:
-  uint8_t code;
   uint32_t argument : 24;
+  uint8_t code;
 
   /**
    * Default constructor.
    * This initializes all the parameters to 0.
    */
-  constexpr error() : code(0), argument(0) {}
+  constexpr error() : argument(0), code(0) {}
 
   /**
    * Constructor for initializing an error from full error code
    * which also includes the argument to the error
    */
-  constexpr error(uint32_t full) : code(full & 0xFF), argument(full >> 8) {}
+  constexpr error(uint32_t full) : argument(full), code(full >> 24) {}
 
   /**
    * Constructor for initializing an error with an error code
    * and an argument.
    */
-  constexpr error(uint8_t code, uint32_t argument) : code(code), argument(argument) {}
+  constexpr error(uint8_t code, uint32_t argument) : argument(argument), code(code) {}
 
   /**
    * Constructor for initializing an error with an error code
    * and another error as an argument.
    *
-   * Note that 8 MSBs of the argument error will be lost.
+   * Note that 8 LSBs of the argument error will be lost.
    */
-  constexpr error(uint8_t code, const error &other) : code(code), argument(other.code | other.argument << 8) {}
+  constexpr error(uint8_t code, const error &other) : argument((static_cast<uint32_t>(other.code) << 16) | (other.argument >> 8)), code(code) {}
 
   /**
    * Copy constructor
    */
-  constexpr error(const error &other) : code(other.code), argument(other.argument) {}
+  constexpr error(const error &other) : argument(other.argument), code(other.code) {}
 
   /**
    * This is used to create an error class which indicates
@@ -74,7 +74,7 @@ public:
    * Cast operator for bool
    */
   constexpr operator bool() const {
-    return this->code != 0 || this->argument != 0;
+    return this->code || this->argument;
   }
 
   /**
